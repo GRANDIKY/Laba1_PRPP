154db39e620ce4e8bb2887d77c1731fa8ac66679
GRANDIKY <mukaunitaz@gmail.com>
2024-04-08 09:06:34 +0300

    Добавьте файлы проекта.

---
+++ Laba1_PRPP/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите N (максимум): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите x (-1 <= x <= 1): ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+double sum = 0;
+
+for (int i = 0; i <= N; i++)
+{
+    sum += (Math.Pow(-1, i) * Math.Pow(x, i + 1)) / (i + 1);
+}
+
+Console.WriteLine($"Сумма равна: {sum}");
